e64153e576cca96b58dedb9dbe0cfe3b550e6d14
Michael Hartung <michael.hartung@pines-technologies.com>
2017-07-19 16:32:36 +0200

    Let's clean up the gruntfile... - MH - PT

--- tasks/html_head_urls_min_toggle.js
+++ tasks/html_head_urls_min_toggle.js
@@ -11,113 +11,56 @@
 module.exports = function(grunt) {
 	// Please see the Grunt documentation for more information regarding task
 	// creation: http://gruntjs.com/creating-tasks
-	grunt
-	    .registerMultiTask(
-	        'html_head_urls_min_toggle',
-	        'Point the html-head href and src urls to minified sources and vice versa.',
-	        function() {
-		        // grunt.log.write("\n" + JSON.stringify(this) + "\n\n");
-		        var global_functions = {};
-		        global_functions = require('./html_head_urls_min_toggle__global_functions.js');
-		        var reference_options_object = {
-		          action : [ 'switch', 'clean' ],
-		          direction : [ 'min', 'regular' ],
-		          file_source : [ 'wildcard', 'list' ]
-		        };
-		        if (global_functions.private_action_checker(this, reference_options_object, true)) {
-			        if (this.data.options.action === 'clean') {
-				        console.log("CLEANING SECTION...!");
-			        }
-			        if (this.data.options.action === 'switch') {
-				        if (this.data.options.file_source === 'list') {
-					        console.log("SWITCHING SECTION LIST...!");
-					        for (var i = 0; i < this.data.custom_files.length; i++) {
-						        console.log(this.data.custom_files[i]);
-					        }
-				        } else {
-					        console.log("SWITCHING SECTION WILDCARD...!");
-					        for (var i = 0; i < this.data.custom_files.length; i++) {
-						        if (this.data.custom_files[i].hasOwnProperty('cwd') && this.data.custom_files[i].hasOwnProperty('src')) {
-							        var current_files_array = global_functions.process_wildcard_input(this.data.custom_files[i].src,
-							            this.data.custom_files[i].cwd);
-							        for (var j = 0; j < current_files_array.length; j++) {
+	grunt.registerMultiTask('html_head_urls_min_toggle',
+	    'Point the html-head href and src urls to minified sources and vice versa.', function() {
+		    // grunt.log.write("\n" + JSON.stringify(this) + "\n\n");
+		    var global_functions = {};
+		    global_functions = require('./html_head_urls_min_toggle__global_functions.js');
+		    var reference_options_object = {
+		      action : [ 'switch', 'clean' ],
+		      direction : [ 'min', 'regular' ],
+		      file_source : [ 'wildcard', 'list' ]
+		    };
+		    if (global_functions.private_action_checker(this, reference_options_object, true)) {
+			    if (this.data.options.action === 'clean') {
+				    console.log("CLEANING SECTION...!");
+			    }
+			    if (this.data.options.action === 'switch') {
+				    if (this.data.options.file_source === 'list') {
+					    console.log("SWITCHING SECTION LIST...!");
+					    for (var i = 0; i < this.data.custom_files.length; i++) {
+						    grunt.log.write("\t" + (i + 1) + ": \"" + this.data.custom_files[i] + "\"");
+						    if (global_functions.toggle_all_head_links(this.data.options.direction, this.data.custom_files[i])) {
+							    grunt.log.write("\t- to \""['green'] + this.data.options.direction['green'] + "\" o.k."['green'] + "\n");
+						    } else {
+							    grunt.log.write("\t- to \""['red'] + this.data.options.direction['red'] + "\">>> Failed...! <<<"['red'] +
+							        "\n");
+						    }
+					    }
+				    } else {
+					    console.log("SWITCHING SECTION WILDCARD...!");
+					    for (var i = 0; i < this.data.custom_files.length; i++) {
+						    if (this.data.custom_files[i].hasOwnProperty('cwd') && this.data.custom_files[i].hasOwnProperty('src')) {
+							    var current_files_array = global_functions.process_wildcard_input(this.data.custom_files[i].src,
+							        this.data.custom_files[i].cwd);
+							    for (var j = 0; j < current_files_array.length; j++) {
 
-								        grunt.log.write("\t" + (j + 1) + ": \"" + current_files_array[j] + "\"");
-								        if (global_functions.toggle_all_head_links(this.data.options.direction, current_files_array[j])) {
-									        grunt.log.write("\t- to \""['green'] + this.data.options.direction['green'] + "\" o.k."['green'] +
-									            "\n");
-								        } else {
-									        grunt.log.write("\t- to \""['red'] + this.data.options.direction['red'] +
-									            "\">>> Failed...! <<<"['red'] + "\n");
-								        }
+								    grunt.log.write("\t" + (j + 1) + ": \"" + current_files_array[j] + "\"");
+								    if (global_functions.toggle_all_head_links(this.data.options.direction, current_files_array[j])) {
+									    grunt.log.write("\t- to \""['green'] + this.data.options.direction['green'] + "\" o.k."['green'] +
+									        "\n");
+								    } else {
+									    grunt.log.write("\t- to \""['red'] + this.data.options.direction['red'] +
+									        "\">>> Failed...! <<<"['red'] + "\n");
+								    }
 
-							        }
-						        } else {
-							        console.log("MISSING NEEDED \"WILDCARD\" FILE PROPERTIES...!");
-						        }
-					        }
-				        }
-			        }
-		        }
-
-		        var min_targets_array = [];
-		        var regular_targets_array = [];
-		        try {
-			        min_targets_array = this.data.min_targets;
-			        if (typeof min_targets_array === 'undefined') {
-				        min_targets_array = [];
-			        }
-		        } catch (exception) {
-			        min_targets_array = [];
-		        }
-		        try {
-			        regular_targets_array = this.data.regular_targets;
-			        if (typeof regular_targets_array === 'undefined') {
-				        regular_targets_array = [];
-			        }
-		        } catch (exception) {
-			        regular_targets_array = [];
-		        }
-		        if (min_targets_array.length === 1) {
-			        grunt.log.write("\nOne file specified to switch all it's \"head links\" to \"minified sources\"...:\n");
-		        } else {
-			        grunt.log.write("\n" + min_targets_array.length +
-			            " Files specified to switch all their \"head links\" to \"minified sources\"...:\n");
-		        }
-
-		        if (min_targets_array.length > 0) {
-			        for (var i = 0; i < min_targets_array.length; i++) {
-				        grunt.log.write("\t" + (i + 1) + ": \"" + min_targets_array[i] + "\"");
-				        if (global_functions.toggle_all_head_links('min', min_targets_array[i])) {
-					        grunt.log.write("\t- to \""['green'] + "min"['green'] + "\" o.k."['green'] + "\n");
-				        } else {
-					        grunt.log.write("\t- to \""['red'] + "min"['red'] + "\" >>> Failed...! <<<"['red'] + "\n");
-				        }
-			        }
-		        } else {
-			        grunt.log
-			            .write("\n>>> NO \"HTML Files\" listed to switch their \"head links\" to \"minified sources\"... <<<\n");
-		        }
-		        grunt.log.write("\n");
-
-		        if (regular_targets_array.length > 0) {
-			        if (regular_targets_array.length === 1) {
-				        grunt.log.write("One file specified to switch all it's \"head links\" to \"regular sources\"...\n");
-			        } else {
-				        grunt.log.write(regular_targets_array.length +
-				            " Files specified to switch all their \"head links\" to \"regular sources\"...\n");
-			        }
-			        for (var j = 0; j < regular_targets_array.length; j++) {
-				        grunt.log.write("\t" + (j + 1) + ": \"" + regular_targets_array[j] + "\"");
-				        if (global_functions.toggle_all_head_links('regular', regular_targets_array[j])) {
-					        grunt.log.write("\t- to \""['green'] + "regular"['green'] + "\" o.k."['green'] + "\n");
-				        } else {
-					        grunt.log.write("\t- to \""['red'] + "regular"['red'] + "\" >>> Failed...! <<<"['red'] + "\n");
-				        }
-			        }
-		        } else {
-			        grunt.log
-			            .write("\n>>> NO \"HTML Files\" listed to switch their \"head links\" to \"regular sources\"... <<<\n");
-		        }
-	        });
+							    }
+						    } else {
+							    console.log("MISSING NEEDED \"WILDCARD\" FILE PROPERTIES...!");
+						    }
+					    }
+				    }
+			    }
+		    }
+	    });
 };
