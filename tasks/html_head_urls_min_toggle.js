b017190e2aeae9f765a218abf7fd0dfabb43d53f
Michael Hartung <michael.hartung@pines-technologies.com>
2017-07-19 16:59:41 +0200

    Removed the is already defined "bugs"... - MH - PT

--- tasks/html_head_urls_min_toggle.js
+++ tasks/html_head_urls_min_toggle.js
@@ -11,89 +11,94 @@
 module.exports = function(grunt) {
 	// Please see the Grunt documentation for more information regarding task
 	// creation: http://gruntjs.com/creating-tasks
-	grunt.registerMultiTask('html_head_urls_min_toggle',
-	    'Point the html-head href and src urls to minified sources and vice versa.', function() {
-		    // grunt.log.write("\n" + JSON.stringify(this) + "\n\n");
-		    var global_functions = {};
-		    global_functions = require('./html_head_urls_min_toggle__global_functions.js');
-		    var reference_options_object = {
-		      action : [ 'switch', 'clean' ],
-		      direction : [ 'min', 'regular' ],
-		      file_source : [ 'wildcard', 'list' ]
-		    };
-		    if (global_functions.private_action_checker(this, reference_options_object, true)) {
-			    if (this.data.options.action === 'clean') {
-				    console.log("CLEANING SECTION...!");
-				    if (this.data.options.file_source === 'list') {
-					    console.log("CLEANING SECTION LIST...!");
-					    for (var i = 0; i < this.data.custom_files.length; i++) {
-						    grunt.log.write("\t" + (i + 1) + ": \"" + this.data.custom_files[i] + "\"");
-						    if (global_functions.toggle_all_head_links(this.data.options.direction, this.data.custom_files[i])) {
-							    grunt.log.write("\t- to \""['green'] + this.data.options.direction['green'] + "\" o.k."['green'] + "\n");
-						    } else {
-							    grunt.log.write("\t- to \""['red'] + this.data.options.direction['red'] + "\">>> Failed...! <<<"['red'] +
-							        "\n");
-						    }
-					    }
-				    } else {
-					    console.log("CLEANING SECTION WILDCARD...!");
-					    for (var k = 0; k < this.data.custom_files.length; k++) {
-						    if (this.data.custom_files[k].hasOwnProperty('cwd') && this.data.custom_files[k].hasOwnProperty('src')) {
-							    var current_files_array = global_functions.process_wildcard_input(this.data.custom_files[k].src,
-							        this.data.custom_files[k].cwd);
-							    for (var j = 0; j < current_files_array.length; j++) {
+	grunt
+	    .registerMultiTask(
+	        'html_head_urls_min_toggle',
+	        'Point the html-head href and src urls to minified sources and vice versa.',
+	        function() {
+		        // grunt.log.write("\n" + JSON.stringify(this) + "\n\n");
+		        var global_functions = {};
+		        global_functions = require('./html_head_urls_min_toggle__global_functions.js');
+		        var reference_options_object = {
+		          action : [ 'switch', 'clean' ],
+		          direction : [ 'min', 'regular' ],
+		          file_source : [ 'wildcard', 'list' ]
+		        };
+		        if (global_functions.private_action_checker(this, reference_options_object, true)) {
+			        if (this.data.options.action === 'clean') {
+				        console.log("CLEANING SECTION...!");
+				        if (this.data.options.file_source === 'list') {
+					        console.log("CLEANING SECTION LIST...!");
+					        for (var i = 0; i < this.data.custom_files.length; i++) {
+						        grunt.log.write("\t" + (i + 1) + ": \"" + this.data.custom_files[i] + "\"");
+						        if (global_functions.toggle_all_head_links(this.data.options.direction, this.data.custom_files[i])) {
+							        grunt.log.write("\t- to \""['green'] + this.data.options.direction['green'] + "\" o.k."['green'] +
+							            "\n");
+						        } else {
+							        grunt.log.write("\t- to \""['red'] + this.data.options.direction['red'] +
+							            "\">>> Failed...! <<<"['red'] + "\n");
+						        }
+					        }
+				        } else {
+					        console.log("CLEANING SECTION WILDCARD...!");
+					        for (var k = 0; k < this.data.custom_files.length; k++) {
+						        if (this.data.custom_files[k].hasOwnProperty('cwd') && this.data.custom_files[k].hasOwnProperty('src')) {
+							        var current_files_array = global_functions.process_wildcard_input(this.data.custom_files[k].src,
+							            this.data.custom_files[k].cwd);
+							        for (var j = 0; j < current_files_array.length; j++) {
+								        grunt.log.write("\t" + (j + 1) + ": \"" + current_files_array[j] + "\"");
+								        if (global_functions.toggle_all_head_links(this.data.options.direction, current_files_array[j])) {
+									        grunt.log.write("\t- to \""['green'] + this.data.options.direction['green'] + "\" o.k."['green'] +
+									            "\n");
+								        } else {
+									        grunt.log.write("\t- to \""['red'] + this.data.options.direction['red'] +
+									            "\">>> Failed...! <<<"['red'] + "\n");
+								        }
+							        }
+						        } else {
+							        console.log("MISSING NEEDED \"WILDCARD\" FILE PROPERTIES...!");
+						        }
+					        }
+				        }
+			        }
 
-								    grunt.log.write("\t" + (j + 1) + ": \"" + current_files_array[j] + "\"");
-								    if (global_functions.toggle_all_head_links(this.data.options.direction, current_files_array[j])) {
-									    grunt.log.write("\t- to \""['green'] + this.data.options.direction['green'] + "\" o.k."['green'] +
-									        "\n");
-								    } else {
-									    grunt.log.write("\t- to \""['red'] + this.data.options.direction['red'] +
-									        "\">>> Failed...! <<<"['red'] + "\n");
-								    }
-							    }
-						    } else {
-							    console.log("MISSING NEEDED \"WILDCARD\" FILE PROPERTIES...!");
-						    }
-					    }
-				    }
-			    }
+			        if (this.data.options.action === 'switch') {
+				        if (this.data.options.file_source === 'list') {
+					        console.log("SWITCHING SECTION LIST...!");
+					        for (var l = 0; l < this.data.custom_files.length; l++) {
+						        grunt.log.write("\t" + (l + 1) + ": \"" + this.data.custom_files[l] + "\"");
+						        if (global_functions.toggle_all_head_links(this.data.options.direction, this.data.custom_files[l])) {
+							        grunt.log.write("\t- to \""['green'] + this.data.options.direction['green'] + "\" o.k."['green'] +
+							            "\n");
+						        } else {
+							        grunt.log.write("\t- to \""['red'] + this.data.options.direction['red'] +
+							            "\">>> Failed...! <<<"['red'] + "\n");
+						        }
+					        }
+				        } else {
+					        console.log("SWITCHING SECTION WILDCARD...!");
+					        for (var m = 0; m < this.data.custom_files.length; m++) {
+						        if (this.data.custom_files[m].hasOwnProperty('cwd') && this.data.custom_files[m].hasOwnProperty('src')) {
+							        var current_second_files_array = global_functions.process_wildcard_input(
+							            this.data.custom_files[m].src, this.data.custom_files[m].cwd);
+							        for (var n = 0; n < current_second_files_array.length; n++) {
 
-			    if (this.data.options.action === 'switch') {
-				    if (this.data.options.file_source === 'list') {
-					    console.log("SWITCHING SECTION LIST...!");
-					    for (var i = 0; i < this.data.custom_files.length; i++) {
-						    grunt.log.write("\t" + (i + 1) + ": \"" + this.data.custom_files[i] + "\"");
-						    if (global_functions.toggle_all_head_links(this.data.options.direction, this.data.custom_files[i])) {
-							    grunt.log.write("\t- to \""['green'] + this.data.options.direction['green'] + "\" o.k."['green'] + "\n");
-						    } else {
-							    grunt.log.write("\t- to \""['red'] + this.data.options.direction['red'] + "\">>> Failed...! <<<"['red'] +
-							        "\n");
-						    }
-					    }
-				    } else {
-					    console.log("SWITCHING SECTION WILDCARD...!");
-					    for (var k = 0; k < this.data.custom_files.length; k++) {
-						    if (this.data.custom_files[k].hasOwnProperty('cwd') && this.data.custom_files[k].hasOwnProperty('src')) {
-							    var current_files_array = global_functions.process_wildcard_input(this.data.custom_files[k].src,
-							        this.data.custom_files[k].cwd);
-							    for (var j = 0; j < current_files_array.length; j++) {
-
-								    grunt.log.write("\t" + (j + 1) + ": \"" + current_files_array[j] + "\"");
-								    if (global_functions.toggle_all_head_links(this.data.options.direction, current_files_array[j])) {
-									    grunt.log.write("\t- to \""['green'] + this.data.options.direction['green'] + "\" o.k."['green'] +
-									        "\n");
-								    } else {
-									    grunt.log.write("\t- to \""['red'] + this.data.options.direction['red'] +
-									        "\">>> Failed...! <<<"['red'] + "\n");
-								    }
-							    }
-						    } else {
-							    console.log("MISSING NEEDED \"WILDCARD\" FILE PROPERTIES...!");
-						    }
-					    }
-				    }
-			    }
-		    }
-	    });
+								        grunt.log.write("\t" + (n + 1) + ": \"" + current_second_files_array[n] + "\"");
+								        if (global_functions.toggle_all_head_links(this.data.options.direction,
+								            current_second_files_array[n])) {
+									        grunt.log.write("\t- to \""['green'] + this.data.options.direction['green'] + "\" o.k."['green'] +
+									            "\n");
+								        } else {
+									        grunt.log.write("\t- to \""['red'] + this.data.options.direction['red'] +
+									            "\">>> Failed...! <<<"['red'] + "\n");
+								        }
+							        }
+						        } else {
+							        console.log("MISSING NEEDED \"WILDCARD\" FILE PROPERTIES...!");
+						        }
+					        }
+				        }
+			        }
+		        }
+	        });
 };
