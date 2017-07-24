e1cab29e4d76043f8f55e2a6b86fa63d84f1aa72
Michael Hartung <michael.hartung@pines-technologies.com>
2017-07-24 14:19:23 +0200

    Works... - MH - PT

--- bin/objects_carver.js
+++ bin/objects_carver.js
@@ -8,13 +8,14 @@
 
 'use strict';
 
-var help_needed__RegExp = new RegExp("^\\s*--?h(elp)?\\s*$", "i");
+var help_needed__RegExp = new RegExp("^--?h(elp)?$", "i");
 
 function scan_arguments(value, index, array) {
 	if (index > 1) {
 		var result_array = help_needed__RegExp.exec(value);
 		if (result_array !== null) {
-			console.log(result_array[0]);
+			// console.log(result_array[0]);
+			// console.log(array[index]);
 		}
 	}
 }
