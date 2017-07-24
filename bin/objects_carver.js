5694f86fab81e894a1f35f1234e3e1d76543b26e
Michael Hartung <michael.hartung@pines-technologies.com>
2017-07-24 14:36:17 +0200

    Nice, short and working... - MH - PT

--- bin/objects_carver.js
+++ bin/objects_carver.js
@@ -8,16 +8,24 @@
 
 'use strict';
 
-var help_needed__RegExp = new RegExp("^--?h(elp)?$", "i");
+var help_needed = false;
+var help_needed__pattern = /^--?h(elp)?$/i;
 
 function scan_arguments(value, index, array) {
 	if (index > 1) {
-		var result_array = help_needed__RegExp.exec(value);
-		if (result_array !== null) {
-			// console.log(result_array[0]);
-			// console.log(array[index]);
+		if (value.search(help_needed__pattern) !== -1) {
+			throw exception;
 		}
 	}
 }
+try {
+	process.argv.forEach(scan_arguments);
+} catch (exception) {
+	help_needed = true;
+}
 
-process.argv.forEach(scan_arguments);
+if (help_needed) {
+	console.log("HELP NEEDED...!");
+} else {
+	console.log("NO HELP NEEDED...!");
+}
