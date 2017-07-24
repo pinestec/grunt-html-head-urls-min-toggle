f81cac7daf3ea943eb9f0ea62e372739f819ca9d
Michael Hartung <michael.hartung@pines-technologies.com>
2017-07-24 16:47:10 +0200

    Shorter... - MH - PT

--- bin/objects_carver.js
+++ bin/objects_carver.js
@@ -8,9 +8,19 @@
 
 'use strict';
 
-var help_needed = false;
-var help_needed__pattern = /^--?h(elp)?$/i;
+function global_help_output() {
+	var plain_file_name_extractor_RegExp = new RegExp("([^/\\\\]*)$");
+	var result_array = plain_file_name_extractor_RegExp.exec(process.argv[1]);
+	if (result_array !== null) {
+		console.log("usage:\t" + result_array[1] + " entire_filename [-h|--help] \n");
+		console.log("Writing internally defined \"reference objects\" to disk with the help of \"JSON.stringify\"...");
+		console.log("Just for later use with other packages.\n");
+		console.log("optional arguments:");
+		console.log("-h, --help\t\tShow this help message and exit");
+	}
+}
 
+var help_needed__pattern = /^--?h(elp)?$/i;
 function scan_arguments(value, index, array) {
 	if (index > 1) {
 		if (value.search(help_needed__pattern) !== -1) {
@@ -21,17 +31,5 @@ function scan_arguments(value, index, array) {
 try {
 	process.argv.forEach(scan_arguments);
 } catch (exception) {
-	help_needed = true;
-}
-
-if (help_needed) {
-	var plain_file_name_extractor_RegExp = new RegExp("([^/\\\\]*)$");
-	var result_array = plain_file_name_extractor_RegExp.exec(process.argv[1]);
-	if (result_array !== null) {
-		console.log("usage:\t" + result_array[1] + " entire_filename [-h|--help] \n");
-		console.log("Writing internally defined \"reference objects\" to disk with the help of \"JSON.stringify\"...");
-		console.log("Just for later use with other packages.\n");
-		console.log("optional arguments:");
-		console.log("-h, --help\t\tShow this help message and exit");
-	}
+	global_help_output();
 }
